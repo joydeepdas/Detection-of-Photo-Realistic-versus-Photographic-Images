7b1d5c8b7c8afe95d5d7ec5fc2f4fe9b1afe109f
JOYDEEP DAS <joydeepdas994@gmail.com>
2017-11-08 17:21:29 +0530

    Delete plot_paper1.m

--- plot_paper1.m
+++
@@ -1,8 +0,0 @@
-natural=importdata('featureori_hist.mat');
-spam=importdata('featurespam_hist.mat');
-fx=1:798;
-fy=1:921;
-plot(fx,natural,'r*',fy,spam,'b+');
-legend('Natural Mean','Spam Mean');
-xlabel('No. of Images');
-ylabel('Lightness');
