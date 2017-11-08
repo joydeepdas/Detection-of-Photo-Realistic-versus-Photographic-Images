2cb76534cdd71634587beb4446fbc578617b5fab
JOYDEEP DAS <joydeepdas994@gmail.com>
2017-11-08 23:02:04 +0530

    Delete test_fft_to_hertz.m

--- PhotoRealistic Videos/codes/test_fft_to_hertz.m
+++
@@ -1,8 +0,0 @@
-Fs = 1e3;
-t = linspace(0,1,1e3);
-x = cos(2*pi*250*t)+randn(size(t));
-xdft = fft(x);
-xdft = xdft(1:length(x)/2+1); % only retaining the positive frequencies
-freq = 0:Fs/length(x):500; % frequency vector from 0 to the Nyquist
-[~,maxindex] = max(abs(xdft));
-fprintf('The maximum occurs at %2.1f Hz\n', freq(maxindex));
