512c72592da49f73db7330776936c8079885cea8
JOYDEEP DAS <joydeepdas994@gmail.com>
2017-11-08 18:20:47 +0530

    Add files via upload

---
+++ PhotoRealistic Images/codes/imageExtraction.m
@@ -0,0 +1,14 @@
+clear all;
+data_location = 'ucid.v2/';
+imagefiles = dir([data_location '*.tif']);
+ii=1;
+for i = 1:length(imagefiles)
+    filename = [data_location imagefiles(i).name];
+    images{i}= (imread(filename));
+    image=cell2mat(images(i));
+    [m,n,o]=size(image);
+    if o==3
+        saveimage{ii}=image;
+        ii=ii+1;
+    end
+end
